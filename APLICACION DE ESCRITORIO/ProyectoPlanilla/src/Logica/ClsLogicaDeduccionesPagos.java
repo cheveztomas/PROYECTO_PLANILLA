3e1e501aa24fb90e1cef934c5ac47d69173c719a
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-18 16:15:21 -0600

    v

---
+++ APLICACION DE ESCRITORIO/ProyectoPlanilla/src/Logica/ClsLogicaDeduccionesPagos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import Entidades.ClsDeduccionesPagos;
+import Entidades.ClsRetorno;
+
+/**
+ *
+ * @author Thomas Chevez
+ */
+public class ClsLogicaDeduccionesPagos {
+    public ClsRetorno GuardarDeduccionesPagos(ClsDeduccionesPagos pvo_DeduccionesPagos){
+        //Variables
+        
+    }
+}
