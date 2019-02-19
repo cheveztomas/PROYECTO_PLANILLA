b8cb58b6bee309cc07abf13ae6741d7a65f3b553
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-19 17:00:18 -0600

    v

--- APLICACION DE ESCRITORIO/ProyectoPlanilla/src/AccesoDatos/ClsADPlanilla.java
+++ APLICACION DE ESCRITORIO/ProyectoPlanilla/src/AccesoDatos/ClsADPlanilla.java
@@ -12,6 +12,7 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Types;
 
 /**
@@ -34,16 +35,22 @@ public class ClsADPlanilla {
             throw e;
         }
     }
-    
+
     public ClsRetorno GenerarPlanilla(ClsPlanilla pvo_Planilla) throws SQLException {
         //Variables
-        ResultSet vlo_RS;
+        ResultSet vlo_RSEmpladosInf;
+        ResultSet vlo_RSPagos;
+        ResultSet vlo_RSDeducciones;
         CallableStatement vlo_CS;
         ClsRetorno vlo_Retorno = new ClsRetorno();
+        String vlc_SentenciaSQL;
+        Statement vlo_Statement;
 
         //Inicio
         try {
             vgo_Connection.setAutoCommit(false);
+
+            //Se inserta una nueva planilla.
             vlo_CS = vgo_Connection.prepareCall("{call SP_GUARDAR_PLANILLA(?,?)}");
             vlo_CS.setInt(1, pvo_Planilla.getVgn_idPlanilla());
             vlo_CS.setDate(2, pvo_Planilla.getVgf_Fecha());
@@ -52,6 +59,27 @@ public class ClsADPlanilla {
             vlo_CS.executeUpdate();
             vlo_Retorno.setVgc_ID(vlo_CS.getInt(1));
             vlo_Retorno.setVgc_Mensaje(vlo_CS.getString(2));
+
+            //Se estable la sentencia sql
+            vlc_SentenciaSQL = "SELECT EMPLEADOS.ID_EMPLEADO,SALARIO_BASE,PRESTAMO,PENSION,CATEGORIA_PUESTO FROM EMPLEADOS INNER JOIN EMPLEADOS_PUESTOS ON EMPLEADOS.ID_EMPLEADO = EMPLEADOS_PUESTOS.ID_EMPLEADO INNER JOIN PUESTOS ON EMPLEADOS_PUESTOS.ID_PUESTO=PUESTOS.ID_PUESTO WHERE EMPLEADOS.BORRADO_LOGICO=0";
+
+            //Se lee toda la información de todos los usuarios
+            vlo_Statement = vgo_Connection.createStatement();
+            vlo_RSEmpladosInf = vlo_Statement.executeQuery(vlc_SentenciaSQL);
+
+            //Se establese la nueva sentencia sql
+            vlc_SentenciaSQL = "SELECT ID_DEDUCCION_PAGO,DEDUCCION_GENERAL,ES_DEDUCCION,TIPO,MONTO FROM DEDUCCIONES_PAGOS WHERE ES_DEDUCCION=1";
+
+            //Se obtinen todas las deducciones existentes.
+            vlo_Statement = vgo_Connection.createStatement();
+            vlo_RSDeducciones = vlo_Statement.executeQuery(vlc_SentenciaSQL);
+
+            //Se establese la nueva sentencia sql
+            vlc_SentenciaSQL = "SELECT ID_DEDUCCION_PAGO,DEDUCCION_GENERAL,TIPO,MONTO FROM DEDUCCIONES_PAGOS WHERE ES_DEDUCCION=0";
+
+            //Se obtiene la lista de pagos existentes
+            vlc_SentenciaSQL = "SELECT ID_DEDUCCION_PAGO,DEDUCCION_GENERAL,TIPO,MONTO FROM DEDUCCIONES_PAGOS WHERE ES_DEDUCCION=0";
+
             vgo_Connection.commit();
         } catch (Exception e) {
             vgo_Connection.rollback();
