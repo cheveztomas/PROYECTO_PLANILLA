d932e6840d4b318feba294ae9d31e46fc35b8040
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-20 16:21:39 -0600

    v

--- APLICACION DE ESCRITORIO/ProyectoPlanilla/src/Presentacion/frmPlanilla.java
+++ APLICACION DE ESCRITORIO/ProyectoPlanilla/src/Presentacion/frmPlanilla.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.JOptionPane;
+import javax.swing.event.TableColumnModelListener;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -27,6 +28,8 @@ public class frmPlanilla extends javax.swing.JInternalFrame {
         initComponents();
         this.closable = true;
         txt_idPlanillas.setVisible(false);
+        jTable1.setVisible(false);
+        //jTable1.setTableHeader(null);
     }
 
     private void CargarListaDetallesPlanillas() {
@@ -38,8 +41,8 @@ public class frmPlanilla extends javax.swing.JInternalFrame {
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
-
         };
+        TableColumnModelListener CModel;
 
         //Inicio
         jTable1.setModel(Modelo);
@@ -49,7 +52,8 @@ public class frmPlanilla extends javax.swing.JInternalFrame {
         Modelo.addColumn("Salario Bruto");
 
         Object[] fila = new Object[4];
-
+        
+       //jTable1.settabl;
         try {
             vlo_RS = vlo_LogicaPlanilla.ListaDetallesPLanilla(Integer.parseInt(txt_idPlanillas.getText()));
             while (vlo_RS.next()) {
@@ -162,21 +166,27 @@ public class frmPlanilla extends javax.swing.JInternalFrame {
         ClsPlanilla vlo_Planilla;
 
         //Inicio
-        try {
-            vlo_Planilla = new ClsPlanilla();
-            vlo_Planilla.setVgn_idPlanilla(-1);
-            String fechaString = new SimpleDateFormat("yyyyMMdd").format(jDateChooser2.getDate());
-            SimpleDateFormat formato = new SimpleDateFormat("yyyyMMdd");
-            Date parsed = (Date) formato.parse(fechaString);
-            java.sql.Date sql = new java.sql.Date(parsed.getTime());
-            vlo_Planilla.setVgf_Fecha(sql);
+        if (jDateChooser2.getDate() == null) {
+            JOptionPane.showMessageDialog(this, "Seleccione la fecha para la planilla.");
+        } else {
+            try {
+                vlo_Planilla = new ClsPlanilla();
+                vlo_Planilla.setVgn_idPlanilla(-1);
+                String fechaString = new SimpleDateFormat("yyyyMMdd").format(jDateChooser2.getDate());
+                SimpleDateFormat formato = new SimpleDateFormat("yyyyMMdd");
+                Date parsed = (Date) formato.parse(fechaString);
+                java.sql.Date sql = new java.sql.Date(parsed.getTime());
+                vlo_Planilla.setVgf_Fecha(sql);
 
-            vlo_Retorno = vlo_LogicaPlanilla.GenerarPlanilla(vlo_Planilla);
-            JOptionPane.showMessageDialog(this, vlo_Retorno.getVgc_Mensaje());
-            txt_idPlanillas.setText(Integer.toString(vlo_Retorno.getVgc_ID()));
-            CargarListaDetallesPlanillas();
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, e.getMessage());
+                vlo_Retorno = vlo_LogicaPlanilla.GenerarPlanilla(vlo_Planilla);
+                JOptionPane.showMessageDialog(this, vlo_Retorno.getVgc_Mensaje());
+                txt_idPlanillas.setText(Integer.toString(vlo_Retorno.getVgc_ID()));
+                jTable1.setVisible(true);
+                //jTable1.setTableHeader(null);
+                CargarListaDetallesPlanillas();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(this, e.getMessage());
+            }
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
