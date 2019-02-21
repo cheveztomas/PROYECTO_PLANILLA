1f71951a8d04e4d33414f143600d1f0751e58dd0
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-21 10:18:24 -0600

    v

--- APLICACION DE ESCRITORIO/ProyectoPlanilla/src/AccesoDatos/ClsADInformacionAcademica.java
+++ APLICACION DE ESCRITORIO/ProyectoPlanilla/src/AccesoDatos/ClsADInformacionAcademica.java
@@ -6,7 +6,11 @@
 package AccesoDatos;
 
 import Configuracion.ClsConexion;
+import Entidades.ClsInformacionAcademica;
+import Entidades.ClsRetorno;
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Types;
 
 /**
  *
@@ -27,5 +31,31 @@ public class ClsADInformacionAcademica {
         } catch (Exception e) {
             throw e;
         }
+    }
+
+    public ClsRetorno GuardarInformacionAcademica(ClsInformacionAcademica pvo_inInformacionAcademica) {
+        //variables
+        ClsRetorno vlo_Retorno = new ClsRetorno();
+        CallableStatement vlo_CS;
+
+        //Inicio
+        try {
+            //Se invoca el procedimiento almacenado de la base de datos.
+            vlo_CS = vgo_Connection.prepareCall("{call SP_GUARDAR_INFORMACION_ACADEMICA(?,?,?,?,?,?)}");
+            
+            //Se setean los valores al procedimiento.
+            vlo_CS.setInt(1, pvo_inInformacionAcademica.getVgn_idInformacionA());
+            vlo_CS.setInt(2, pvo_inInformacionAcademica.getVgn_idEmpleado());
+            vlo_CS.setString(3, pvo_inInformacionAcademica.getVgc_especialidad());
+            vlo_CS.setString(4, pvo_inInformacionAcademica.getVgc_informacion());
+            vlo_CS.setString(5, vlo_Retorno.getVgc_Mensaje());
+            
+            //Se establecen los valroes de salida.
+            vlo_CS.registerOutParameter(1, Types.INTEGER);
+            vlo_CS.registerOutParameter(5, Types.VARCHAR);
+        } catch (Exception e) {
+        }
+
+        return vlo_Retorno;
     }
 }
