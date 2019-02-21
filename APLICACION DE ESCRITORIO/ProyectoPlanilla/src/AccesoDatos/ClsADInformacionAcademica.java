14c9b0104e38eae6d52816280ba67eb18a89ae57
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-21 09:26:36 -0600

    v

--- APLICACION DE ESCRITORIO/ProyectoPlanilla/src/AccesoDatos/ClsADInformacionAcademica.java
+++ APLICACION DE ESCRITORIO/ProyectoPlanilla/src/AccesoDatos/ClsADInformacionAcademica.java
@@ -5,10 +5,27 @@
  */
 package AccesoDatos;
 
+import Configuracion.ClsConexion;
+import java.sql.Connection;
+
 /**
  *
  * @author tomas
  */
 public class ClsADInformacionAcademica {
-    
+
+    //Atributos
+    Connection vgo_Connection;
+
+    //Metodos
+    public ClsADInformacionAcademica() {
+        //Se instancia la conexion
+        ClsConexion vlo_ClsConexion = new ClsConexion();
+        try {
+            //Se guarda en una variable global la conexion
+            vgo_Connection = vlo_ClsConexion.ClsConexionBD();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
